a2b8c84c1ef09c25f3b03c5bd44b156c6455bcf8
Your Name <you@example.com>
2021-01-22 15:51:18 +0600

    added new file

---
+++ java.js
@@ -0,0 +1,147 @@
+// let points = 0;
+
+// let answer1 = prompt ("What is  the  capital of Kyrgyz pepublic ?");
+// if (answer1 === "Bishkek"){
+//     points = points + 10;
+// }
+
+
+
+// let answer2 = prompt ("How many districts are there  in Kyrgyzstan?");
+// if (answer2 === "41"){
+//     points = points + 10;
+// }
+// alert("You've earned " + points + " points");
+
+
+
+
+
+// let answer3 = prompt ("Hydrogen formula?");
+// if (answer3 === "H2O"){
+//     points = points + 10;
+// }
+//  alert("You've earned " + points + " points");
+
+
+// let answer4 = prompt ("You are love you?");
+// if (answer4 === ""){
+//    points = points + 10;
+//  }
+//  alert("You've earned " + points + " points");
+
+// let answer3 = prompt (`Author book "Rich Dad and Poor Dad?"`);
+// if (answer3 === "Robert"){
+//      points = points + 10;
+//  }
+
+// let answer4 = prompt ("Square  of Kyrgyzstan?");
+//  if (answer4 === "1990000km"){
+//     points = points + 10;
+//   }
+//   alert("You've earned " + points + " points");
+
+
+
+// let answer5 = prompt ("Bishkek capital of?");
+// if (answer5  == "Kyrgyzstan"){
+// console.log ("True")
+// }
+// else {
+//  console.log ("False");
+// }
+
+
+// let answer6 = prompt ("в чем измеряется температура?");
+// if (answer6 == "C"){
+//   console.log ("true")
+// }
+// else {
+//  console.log( "FAlse")
+// }
+
+// let answer6 = prompt ("в чем измеряется дистанция ?");
+//  if (answer6 == "M"){
+//    console.log ("true")
+//  }
+//  else {
+//  console.log( "FAlse")
+//  }
+// let answer7 = prompt ("в чем измеряется вес?");
+// if (answer7 == "кг"){
+//   console.log ("true")
+// }
+//  else {
+//  console.log( "FAlse")
+//  }
+
+
+// let  unit = prompt ("точка замерзания воды?");
+// let temperature = prompt ("введите градус");
+// if ( unit = "c" && temperature == 0){
+//     console.log ("true")
+//   }
+
+//   if ( unit = "c" && temperature == 273){
+//     console.log ("true")
+//   }
+//     if ( unit = "c" && temperature == 32){
+//     console.log ("true")
+//   }
+// else {
+//   console.log("FAlse")
+// }
+
+// let  unit = prompt ("скорость звука?");
+// let speed = prompt ("введите скорость?");
+// if ( unit == "hm/h"){
+//   if ( speed == 1234 ){
+//       console.log ("true")
+//   }
+// }
+// else {
+//     console.log ("False")
+// }
+//  if ( unit = "m/h"){
+//    if ( speed == 767){
+//       console.log ("true")
+//     }
+// }
+// else {
+//      console.log ("False")
+// }
+//   if ( unit = "m/s"){
+//      if ( speed == 1234){
+//          console.log ("true")
+//   }
+// }
+// else {
+//      console.log ("False")
+// } 
+// let answer5 = prompt ("Столицей какой страны является город Бишкек?");
+//  if (answer5  == "Кыргызстан" || answer5 == "Киризия"){
+//   console.log ("True")
+// }
+// else if (answer5 == "Кыргызская республика" || answer5 == "Республика Кыргызстан" || answer5 == "Кыргыз" ){
+//       console.log ("True");
+//  }
+//  else {
+//    console.log ("False")
+//  }
+ 
+
+
+let country =  prompt (`На каком языке хочешь узать " Новый год"`);
+ switch (country){
+    case "Кыргызстан":
+      console.log ("жаны жылын менен");
+      break;
+
+    case "AKШ":
+       console.log ("Happy new year");
+       break;
+
+    case "Немецкий":
+       console.log ("Neues Jahr!");
+       break;
+}
